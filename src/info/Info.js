f40951b2537b13b2a47f40a56437ac8b51cc09ab
Josh Chappelow <jdchappelow@gmail.com>
2022-09-12 17:15:20 +0900

    added more projects

--- src/info/Info.js
+++ src/info/Info.js
@@ -3,7 +3,7 @@ import pokemonhunter from "../img/pokemonhunter.png";
 import tokyovinyls from "../img/tokyovinyls.png";
 import tofuture from "../img/tofuture.png";
 import slack from "../img/slack.png";
-import mock5 from "../img/mock5.png";
+import shopifypokemon from "../img/shopifypokemon.png";
 
 export let colors = ["#f0ead6", "#548C2F"];
 
@@ -81,24 +81,41 @@ export const info = {
       live: "https://jchapps.github.io/react-pokemon-hunter/",
       source: "https://github.com/jchapps/react-pokemon-hunter",
       image: pokemonhunter,
+      software: "React, JavaScript, CSS3, HTML5 Github.",
+      info: "Find GIFs of your favourite pokemon.",
     },
     {
       title: "Tokyo Vinyls",
       live: "https://tokyo-vinyls.herokuapp.com/",
       source: "https://github.com/jchapps/tokyo-vinyls",
       image: tokyovinyls,
+      software:
+        "Ruby on rails, CSS3, HTML5, Heroku, Bootstrap, Figma,PostGreSQL.",
+      info: "Web app built in 5days to connect vinyl collectors and vinyl fans.",
     },
     {
       title: "toFuture",
       live: "https://www.tofuture.co/",
       source: "https://github.com/jchapps/tofuture",
+      software:
+        "Ruby on rails, JavaScript, CSS3, HTML5, Heroku, Bootstrap, Figma, PostGreSQL.",
+      info: "Web app built in 10days to connect conscious investors to sustainable products.",
       image: tofuture,
     },
     {
       title: "Slack Clone",
       live: "https://jchapps.github.io/react-slack-clone/",
       source: "https://github.com/jchapps/react-slack-clone",
+      software: "React, JavaScript, CSS3, HTML5, Github.",
+      info: "Slack clone built in Slack featuring DoTA2 heroes.",
       image: slack,
+    },
+    {
+      title: "Shopify Pokemon Cards - IN PROGESS",
+      source: "https://github.com/billcookie/shopify-pokemon",
+      software: "Frontend React, Ruby on Rails backend, Shopify integrated.",
+      info: "Web App for pokemon card collectors to buy their favourite cards.",
+      image: shopifypokemon,
     },
   ],
 };
